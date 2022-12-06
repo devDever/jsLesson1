54de314c9e2a66e457fcc30ba7da3d77a34d9305
isa <caesar787898@gmail.com>
2022-12-06 20:07:23 +0600

    f

---
+++ index.js
@@ -0,0 +1,17 @@
+// const age = 22;
+// let name = 'Isa';
+
+const age = 22; // * number
+const name = 'Isa'; // * string
+const isHungry = true; // * boolean - true/false
+const nothing = null; // * null - nothing
+const notFound = undefined; // * not found
+const arr = [1, 2, 3, 4]; // * array on numbers
+
+const person = {
+    name: 'Isa',
+    age: 22,
+    cityName: 'Almaty'
+} // * object
+
+console.log(name);
