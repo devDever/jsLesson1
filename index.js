847591733dd2bca8106032ab12fea5b9ca608ab4
isa <caesar787898@gmail.com>
2022-12-06 21:05:51 +0600

    s

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 // const age = 22;
 // let name = 'Isa';
 
-const age = 22; // * number
-const name = 'Isa'; // * string
-const isHungry = true; // * boolean - true/false
-const nothing = null; // * null - nothing
-const notFound = undefined; // * not found
-const arr = [1, 2, 3, 4]; // * array on numbers
+// const age = 22; // * number
+// const name = 'Isa'; // * string
+// const isHungry = true; // * boolean - true/false
+// const nothing = null; // * null - nothing
+// const notFound = undefined; // * not found
+// const arr = [1, 2, 3, 4]; // * array on numbers
 
-const person = {
-    name: 'Isa',
-    age: 22,
-    cityName: 'Almaty'
-} // * object
+// const person = {
+//     name: 'Isa',
+//     age: 22,
+//     cityName: 'Almaty'
+// } // * object
 
-console.log(name);
+
+// const animals = ['cat', 'dog', 'bird', 'mouse', 'human'];
+
+// * Чуть чуть про функции 
+// function showInConsole() {
+//     alert('message + number')
+// }
+
+// showInConsole();
+
+const x = 5;
+const y = 10;
+
+
+// * математические операторы
+console.log(x + y); // * plus
+console.log(x / y); // * divaid
+console.log(x * y); // * multiply
+console.log(x - y); // * minus
+
+const some = 1 + 1 + 3 + '2';
+console.log(some);
