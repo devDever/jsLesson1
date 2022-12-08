ad07785e6edbff27176d4f8f3762763c5fca30d9
isa <caesar787898@gmail.com>
2022-12-08 19:52:42 +0600

    conditions

--- index.js
+++ index.js
@@ -24,15 +24,70 @@
 
 // showInConsole();
 
+// const x = 5;
+// const y = 10;
+
+
+// // * математические операторы
+// console.log(x + y); // * plus
+// console.log(x / y); // * divaid
+// console.log(x * y); // * multiply
+// console.log(x - y); // * minus
+
+// const some = 1 + 1 + 3 + '2';
+// console.log(some);
+
+// * || или
+// * && и
+
+// * > больше
+// * < меьше
+
+// * == равно
+// * != не равно
+
+
+
+
+// const x = 2;
+// const y = 5;
+
+
+
+// if (x > 3) {
+//     console.log('x is more than 3: ', x);
+// } else {
+//     console.log('x is less than 3: ', x);
+// }
+
+
+// const name = 'Isa';
+
+// if (name == 'Isa') {
+//     console.log('name is Isa')
+// } else {
+//     console.log('not Isa')
+// }
+
+// const banana = 'banana';
+// const apple = 'banana';
+
+// const x = 5;
+
+
+// if (banana == apple) {
+
+//     console.log('is banan');
+
+// } else {
+//     console.log('not banan');
+// }
+
 const x = 5;
-const y = 10;
+const y = 1;
 
-
-// * математические операторы
-console.log(x + y); // * plus
-console.log(x / y); // * divaid
-console.log(x * y); // * multiply
-console.log(x - y); // * minus
-
-const some = 1 + 1 + 3 + '2';
-console.log(some);
+if (x != y) {
+    console.log('x != y');
+} else {
+    console.log('x == y');
+}
